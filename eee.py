d4e3238b322ab077c1c152a4381b6368046bcc22
Anton Luis Guevara <lolplayer1231@outlook.com>
2025-11-08 14:12:37 +0800

    feat: :art: tested turtle py

--- eee.py
+++ eee.py
@@ -3,13 +3,13 @@ import turtle
 import time 
 
 t=turtle.Turtle()
-t.color('red','blue')
+t.color('Green', 'Navy')
 t.pensize (1)
 
 t.begin_fill()
 
-t.circle(1000)
+t.circle(10)
 
 t.end_fill ()
-
+t.write("helloworld", align="center")
 turtle.done()
